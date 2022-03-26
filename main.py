30ff2dc35e9b28b37c318a08f025e142a10fbe53
summus2020 <summus.python@gmail.com>
2022-03-26 13:27:52 +0200

    states added

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from curses import window
 import sys
 from PyQt5 import QtWidgets
 from PyQt5 import uic
+from PyQt5.QtCore import QStateMachine, QState
+from PyQt5.QtCore import QEventTransition, QPropertyAnimation
+from PyQt5.QtCore import QSize, QEasingCurve, QEvent
 
 
 class MyUI(QtWidgets.QMainWindow):
@@ -10,7 +13,59 @@ class MyUI(QtWidgets.QMainWindow):
         super(MyUI, self).__init__()
         uic.loadUi("mainwindow.ui", self)
         
+        self.handle_states()
+        
         self.show()
+    
+    def handle_states(self):
+        # create state machine
+        machine = QStateMachine(self)
+        
+        # create states
+        s1 = QState()
+        s2 = QState()
+        s3 = QState()
+        
+        # assign properties to states
+        # State 1
+        s1.assignProperty(self.lbl, "text", "1")
+        s1.assignProperty(self.frame1, "styleSheet", "background-color: rgb(16, 128, 128);")
+        s1.assignProperty(self.frame1, "maximumSize", QSize(500, 200))
+        # State 2
+        s2.assignProperty(self.lbl, "text", "2")
+        s2.assignProperty(self.frame1, "styleSheet", "background-color: rgb(255, 255, 10);")
+        s2.assignProperty(self.frame1, "maximumSize", QSize(600, 150))
+        # State 3
+        s3.assignProperty(self.lbl, "text", "3")
+        s3.assignProperty(self.frame1, "styleSheet", "background-color: rgb(102, 102, 255);")
+        s3.assignProperty(self.frame1, "maximumSize", QSize(300, 300))
+        
+        # create animation
+        animation = QPropertyAnimation(self.frame1, b"maximumSize")
+        animation.setEasingCurve(QEasingCurve.InOutCubic)
+        animation.setDuration(500)
+        
+        # add event transitions
+        t1 = QEventTransition(self.but, QEvent.MouseButtonPress)
+        t1.setTargetState(s2)
+        t1.addAnimation(animation)
+        s1.addTransition(t1)
+        
+        t2 = QEventTransition(self.but, QEvent.MouseButtonPress)
+        t2.setTargetState(s3)
+        t2.addAnimation(animation)
+        s2.addTransition(t2)
+        
+        t3 = QEventTransition(self.but, QEvent.MouseButtonPress)
+        t3.setTargetState(s1)
+        t3.addAnimation(animation)
+        s3.addTransition(t3)
+        
+        machine.addState(s1)
+        machine.addState(s2)
+        machine.addState(s3)
+        machine.setInitialState(s1)
+        machine.start()
 
 
 def main():
